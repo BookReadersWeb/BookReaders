ff44b06ee2711e7500fb58581efcafd3297accad
RubenSans <rubensanbel@campus.monlau.com>
2024-03-02 15:51:55 +0100

    Sistema de rutas dinamicas

---
+++ public/router.php
@@ -0,0 +1,22 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/BookReaders/' :
+        // Esta sera la landing page
+        require '../app/views/home.php';
+        break;
+    default:    
+        $request = str_replace('/BookReaders/', '', $request);
+        $filename = '../app/views/' . $request . '.php';
+
+        if (file_exists($filename)) {
+            require_once $filename;
+            break;
+        } 
+
+        http_response_code(404);
+        require '../app/views/404.php';        
+        break;
+}
