81323b09784e5434f83539408a3e4a7b3a0036de
RubenSans <rubensanbel@campus.monlau.com>
2024-03-09 13:29:16 +0100

    Update home.php

--- app/views/home.php
+++ app/views/home.php
@@ -21,10 +21,20 @@ if (!isset($_SESSION['userData']) || empty($_SESSION['userData']['username'])) {
     <title>Home</title>    
 </head>
 <body>
-    <h1>Bienvenido <?php echo htmlspecialchars($userData['username']); ?></h1>
-    <form action="logout" method="post" onsubmit="return confirm('¿Estás seguro de que quieres cerrar tu sesión?');">
-        <button type="submit">Cerrar sesión</button>
-    </form>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-body d-flex justify-content-between">
+                        <h2 class="card-title">¡Bienvenido, <?php echo htmlspecialchars($userData['username']); ?>!</h2>
+                        <form action="logout.php" method="post">
+                            <button type="submit" class="btn btn-danger">Cerrar sesión</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
     <script src="lib/jquery-3.7.1/js/jquery-3.7.1.min.js"></script>
     <script src="lib/bootstrap-5.3.3/js/bootstrap.min.js"></script>
