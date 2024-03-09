d78b37bb6a6353fc80df3aa6ae152c2740210a0a
RubenSans <rubensanbel@campus.monlau.com>
2024-03-09 13:26:44 +0100

    Update home.php styles/scripts

--- app/views/home.php
+++ app/views/home.php
@@ -16,6 +16,8 @@ if (!isset($_SESSION['userData']) || empty($_SESSION['userData']['username'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lib/bootstrap-5.3.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Home</title>    
 </head>
 <body>
@@ -23,5 +25,8 @@ if (!isset($_SESSION['userData']) || empty($_SESSION['userData']['username'])) {
     <form action="logout" method="post" onsubmit="return confirm('¿Estás seguro de que quieres cerrar tu sesión?');">
         <button type="submit">Cerrar sesión</button>
     </form>
+    
+    <script src="lib/jquery-3.7.1/js/jquery-3.7.1.min.js"></script>
+    <script src="lib/bootstrap-5.3.3/js/bootstrap.min.js"></script>
 </body>
 </html>
