f3308d5c2199cc31dd84a3f27333c30b8b0be6a1
RubenSans <rubensanbel@campus.monlau.com>
2024-03-10 18:56:21 +0100

    Fix minor bug routing home

--- app/views/home.php
+++ app/views/home.php
@@ -3,7 +3,7 @@
 session_start();
 
 if (!isset($_SESSION['userData']) || empty($_SESSION['userData']['username'])) {
-    header('Location: login.php');
+    header('Location: login');
     exit;
 } else {
     $userData = $_SESSION['userData'];
